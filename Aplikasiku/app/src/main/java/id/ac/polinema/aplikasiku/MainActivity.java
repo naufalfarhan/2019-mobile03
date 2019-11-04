b95506563a42366c0e048d3ae93f6b861ec51b59
naufalfarhan <naufalfarhannn@gmail.com>
2019-11-04 08:22:21 +0700

    aplikasiku

--- Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/MainActivity.java
+++ Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/MainActivity.java
@@ -1,10 +1,10 @@
 package id.ac.polinema.aplikasiku;
 
-import androidx.appcompat.app.AppCompatActivity;
+		import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
+		import android.content.Intent;
+		import android.os.Bundle;
+		import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
